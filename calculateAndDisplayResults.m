120f3aa78262cb5ce6bad82c497df734f47d2a06
hesham <hishammohmed12345@gmail.com>
2023-12-30 21:20:56 +0200

    Feat : add final version

--- calculateAndDisplayResults.m
+++ calculateAndDisplayResults.m
@@ -1,4 +1,4 @@
-function calculateAndDisplayResults(conductorTypeDropdown, bundlingDropdown, bundleNumberInput, radiusInput, spacingInput, phaseConfigDropdown, spacing12Input, spacing23Input, spacing31Input, inductanceLabel, capacitanceLabel , strandedDropdown , spaceBetweenBundlesInput)
+function calculateAndDisplayResults(conductorTypeDropdown, bundlingDropdown, bundleNumberInput, radiusInput, spacingInput, phaseConfigDropdown, spacing12Input, spacing23Input, spacing31Input, inductanceLabel, capacitanceLabel, strandedDropdown, spaceBetweenBundlesInput)
     % Compatibility check for MATLAB version
     if isobject(conductorTypeDropdown) % For newer MATLAB versions (R2014b and later)
         conductorType = conductorTypeDropdown.String{conductorTypeDropdown.Value};
@@ -12,13 +12,20 @@ function calculateAndDisplayResults(conductorTypeDropdown, bundlingDropdown, bun
         bundling = bundling{get(bundlingDropdown, 'Value')};
         phaseConfig = get(phaseConfigDropdown, 'String');
         phaseConfig = phaseConfig{get(phaseConfigDropdown, 'Value')};
-        strandedType = get(strandedDropdown,'String');
+        strandedType = get(strandedDropdown, 'String');
         strandedType = strandedType{get(strandedDropdown, 'Value')};
     end
+
     % Retrieve additional spacing inputs for three-phase configuration
-    spacing12 = str2double(get(spacing12Input,'String'));
-    spacing23 = str2double(get(spacing23Input,'String'));
-    spacing31 = str2double(get(spacing31Input,'String'));
+    spacing12 = str2double(get(spacing12Input, 'String'));
+    spacing23 = str2double(get(spacing23Input, 'String'));
+    spacing31 = str2double(get(spacing31Input, 'String'));
+
+    if (STRCMP(strandedType, 'solid'))
+        conductorType = 'solid';
+        strandedType = '3-Triangle';
+    end
+
     [strandedNum, strandedFactor] = getStrandedNumAndFactor(strandedType);
 
     % Retrieve and validate other inputs
@@ -28,12 +35,12 @@ function calculateAndDisplayResults(conductorTypeDropdown, bundlingDropdown, bun
     spaceBetweenBundles = str2double(get(spaceBetweenBundlesInput, 'String'));
 
     % Validate inputs
-    if isnan(bundleNumber) || isnan(radius) || isnan(spacing) || radius <= 0 || spacing <= 0
+    if isnan(radius) || radius <= 0 || spacing <= 0
         errordlg('Invalid input values. Please enter positive numbers.', 'Input Error');
         return;
     end
-    
-    [L, C] = calculateInductanceCapacitance(conductorType, bundling, bundleNumber, radius, spacing, phaseConfig , strandedNum, strandedFactor , spaceBetweenBundles , spacing12, spacing23, spacing31);
+
+    [L, C] = calculateInductanceCapacitance(conductorType, bundling, bundleNumber, radius, spacing, phaseConfig, strandedNum, strandedFactor, spaceBetweenBundles, spacing12, spacing23, spacing31);
 
     % Update result labels
     set(inductanceLabel, 'String', ['Inductance: ', num2str(L, '%.4e'), ' H/m']);
--- createTransmissionLineCalculator.m
+++ createTransmissionLineCalculator.m
@@ -22,45 +22,45 @@ function createTransmissionLineCalculator()
     uicontrol('Style', 'text', 'Position', [50, 600, 200, 20], 'String', 'Phase Configuration:', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
     phaseConfigDropdown = uicontrol('Style', 'popupmenu', 'Value', 1, 'Position', [260, 600, 200, 20], 'String', {'Single-Phase', 'Single-Phase Two-Wire', 'Three-Phase', 'Three-Phase Double-circuit'}, 'Callback', @(src, event) updateImageDisplay());
 
-    % Dropdown for Conductor Type
-    uicontrol('Style', 'text', 'Position', [50, 560, 200, 20], 'String', 'Conductor Type:', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
-    conductorTypeDropdown = uicontrol('Style', 'popupmenu', 'Position', [260, 560, 200, 20], 'String', {'Solid', 'Stranded'});
+    % % Dropdown for Conductor Type
+    % uicontrol('Style', 'text', 'Position', [50, 560, 200, 20], 'String', 'Conductor Type:', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
+    % conductorTypeDropdown = uicontrol('Style', 'popupmenu', 'Position', [260, 560, 200, 20], 'String', {'Solid', 'Stranded'});
 
     % Dropdown for Conductor Type
-    uicontrol('Style', 'text', 'Position', [50, 520, 200, 20], 'String', 'Stranded Type:', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
-    strandedDropdown = uicontrol('Style', 'popupmenu', 'Position', [260, 520, 200, 20], 'String', {'3-Triangle', '4-Row', '4-Square', '7-Hexagonal', '9-Square'}, 'Callback', @(src, event) updateStrandedImage());
+    uicontrol('Style', 'text', 'Position', [50, 560, 200, 20], 'String', 'Stranded Type:', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
+    strandedDropdown = uicontrol('Style', 'popupmenu', 'Position', [260, 560, 200, 20], 'String', {'Solid','3-Triangle', '4-Row', '4-Square', '7-Hexagonal', '9-Square'}, 'Callback', @(src, event) updateStrandedImage());
 
     % Dropdown for Bundling
-    uicontrol('Style', 'text', 'Position', [50, 480, 200, 20], 'String', 'Bundling Option:', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
-    bundlingDropdown = uicontrol('Style', 'popupmenu', 'Position', [260, 480, 200, 20], 'String', {'Non-Bundled', 'Bundled (<= 3)', 'Bundled (> 3)'}, 'Callback', @(src, event) updateBundlingImage());
+    uicontrol('Style', 'text', 'Position', [50, 520, 200, 20], 'String', 'Bundling Option:', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
+    bundlingDropdown = uicontrol('Style', 'popupmenu', 'Position', [260, 520, 200, 20], 'String', {'Non-Bundled', 'Bundled (<= 3)', 'Bundled (> 3)'}, 'Callback', @(src, event) updateBundlingImage());
 
     % Input for number of conductors in a bundle
-    uicontrol('Style', 'text', 'Position', [50, 440, 200, 20], 'String', 'Bundle Number:', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
-    bundleNumberInput = uicontrol('Style', 'edit', 'Position', [260, 440, 200, 20]);
+    uicontrol('Style', 'text', 'Position', [50, 480, 200, 20], 'String', 'Bundle Number:', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
+    bundleNumberInput = uicontrol('Style', 'edit', 'Position', [260, 480, 200, 20]);
 
-    uicontrol('Style', 'text', 'Position', [50, 400, 200, 20], 'String', 'Space between Bundles (m):', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
-    spaceBetweenBundlesInput = uicontrol('Style', 'edit', 'Position', [260, 400, 200, 20]);
+    uicontrol('Style', 'text', 'Position', [50, 440, 200, 20], 'String', 'Space between Bundles (m):', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
+    spaceBetweenBundlesInput = uicontrol('Style', 'edit', 'Position', [260, 440, 200, 20]);
 
     % Input field for Conductor Radius
-    uicontrol('Style', 'text', 'Position', [50, 360, 200, 20], 'String', 'Conductor Radius (m):', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
-    radiusInput = uicontrol('Style', 'edit', 'Position', [260, 360, 200, 20]);
+    uicontrol('Style', 'text', 'Position', [50, 400, 200, 20], 'String', 'Conductor Radius (m):', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
+    radiusInput = uicontrol('Style', 'edit', 'Position', [260, 400, 200, 20]);
 
     % Input field for General Spacing
-    uicontrol('Style', 'text', 'Position', [50, 320, 200, 20], 'String', 'General Spacing (m):', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
-    spacingInput = uicontrol('Style', 'edit', 'Position', [260, 320, 200, 20]);
+    uicontrol('Style', 'text', 'Position', [50, 360, 200, 20], 'String', 'General Spacing (m):', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
+    spacingInput = uicontrol('Style', 'edit', 'Position', [260, 360, 200, 20]);
 
     % Function to create additional input fields for three-phase configuration
-    uicontrol('Style', 'text', 'Position', [50, 280, 200, 20], 'String', 'Spacing 1-2 or X (m):', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
-    spacing12Input = uicontrol('Style', 'edit', 'Position', [260, 280, 200, 20]);
+    uicontrol('Style', 'text', 'Position', [50, 320, 200, 20], 'String', 'Spacing 1-2 or X (m):', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
+    spacing12Input = uicontrol('Style', 'edit', 'Position', [260, 320, 200, 20]);
 
-    uicontrol('Style', 'text', 'Position', [50, 240, 200, 20], 'String', 'Spacing 2-3 or Y (m):', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
-    spacing23Input = uicontrol('Style', 'edit', 'Position', [260, 240, 200, 20]);
+    uicontrol('Style', 'text', 'Position', [50, 280, 200, 20], 'String', 'Spacing 2-3 or Y (m):', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
+    spacing23Input = uicontrol('Style', 'edit', 'Position', [260, 280, 200, 20]);
 
-    uicontrol('Style', 'text', 'Position', [50, 200, 200, 20], 'String', 'Spacing 1-3 or Z (m):', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
-    spacing31Input = uicontrol('Style', 'edit', 'Position', [260, 200, 200, 20]);
+    uicontrol('Style', 'text', 'Position', [50, 240, 200, 20], 'String', 'Spacing 1-3 or Z (m):', 'HorizontalAlignment', 'left', 'BackgroundColor', [0.95, 0.95, 0.95]);
+    spacing31Input = uicontrol('Style', 'edit', 'Position', [260, 240, 200, 20]);
 
     % Button for calculation, moved down for better spacing
-    calculateButton = uicontrol('Style', 'pushbutton', 'Position', [340, 150, 120, 40], 'String', 'Calculate', 'FontSize', 10, 'FontWeight', 'bold');
+    calculateButton = uicontrol('Style', 'pushbutton', 'Position', [340, 190, 120, 40], 'String', 'Calculate', 'FontSize', 10, 'FontWeight', 'bold');
 
     % Result labels for inductance and capacitance
     inductanceLabel = uicontrol('Style', 'text', 'Position', [50, 110, 700, 20], 'String', 'Inductance: ', 'BackgroundColor', [0.95, 0.95, 0.95], 'FontSize', 10, 'HorizontalAlignment', 'left');
